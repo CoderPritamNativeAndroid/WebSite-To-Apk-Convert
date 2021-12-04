f83b70cab96612d7a44811b1e1f3c957fdbcfb73
Pritam <pritampachal2014@gmail.com>
2021-12-05 00:22:07 +0530

    Commit Successfully

---
+++ app/src/main/java/com/pritampachal/websitetoapkconvert/MainActivity.java
@@ -0,0 +1,41 @@
+package com.pritampachal.websitetoapkconvert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new ClassWebViewClient());
+        webView.loadUrl("https://gitlab.yeshostings.com/");
+    }
+    private static class ClassWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
