e787e022116906ae1469096342b171d79a620a80
CoderPritamNativeAndroid <pritampachal2014@gmail.com>
2021-12-05 17:11:02 +0530

    Commit Successfully

--- app/src/main/java/com/pritampachal/websitetoapkconvert/MainActivity.java
+++ app/src/main/java/com/pritampachal/websitetoapkconvert/MainActivity.java
@@ -9,19 +9,22 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-    private WebView webView;
+    WebView webView;
+    String url;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        url="https://www.google.com/";
         webView=findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.setWebViewClient(new ClassWebViewClient());
-        webView.loadUrl("https://gitlab.yeshostings.com/");
+        webView.loadUrl(url);
     }
+
     private static class ClassWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
